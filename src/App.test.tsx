fb72e6c72f33019a94671d4f28f0f82c39a451c3
Ben Warwick-Champion <ben.w-c@sky.com>
2021-04-02 12:12:05 +0100

    :white_check_mark: Add some simple tests

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,30 @@
-import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
+import userEvent from '@testing-library/user-event';
 
-test('renders learn react link', () => {
+test('title and new task form toggle button visible', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const buttonElement = screen.getByText(/add/i);
+  const h1Element = screen.getByText(/task tracker/i);
+  expect(buttonElement).toBeInTheDocument();
+  expect(h1Element).toBeInTheDocument();
+});
+
+test('add new task form toggles', () => {
+  render(<App />);
+  // Check the button exists and displays 'Add'
+  const formToggleButton = screen.getByRole('button');
+  expect(formToggleButton).toHaveTextContent('Add');
+
+  // Click the toggle
+  userEvent.click(screen.getByRole('button'));
+
+  // Check the form fields are visible
+  const addDateAndTime = screen.getByText(/day and time/i);
+  const setReminder = screen.getByText(/set reminder/i);
+  expect(addDateAndTime).toBeInTheDocument();
+  expect(setReminder).toBeInTheDocument();
+
+  // Assert the toggle button text updated
+  expect(formToggleButton).toHaveTextContent('Close');
 });
